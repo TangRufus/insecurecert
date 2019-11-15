786e1eff24a7b8d9f482c9ba1edaaf8d79517814
TangRufus <tangrufus@gmail.com>
2019-11-15 12:58:41 +0000

    temp

--- cert.go
+++ cert.go
@@ -3,8 +3,11 @@ package main
 //package insecurecert
 
 import (
+	"encoding/pem"
 	"io/ioutil"
 	"crypto/tls"
+	"log"
+	"os"
 	"strconv"
 )
 
@@ -14,7 +17,7 @@ type Cert struct {
 }
 
 func (c Cert) addr() string {
-	return c.host + strconv.Itoa(c.port)
+	return c.host + ":" + strconv.Itoa(c.port)
 }
 
 func (c Cert) der() ([]byte, error) {
@@ -40,7 +43,19 @@ func main() {
 		port: 443,
 	}
 
-	d, _ := c.der()
+	d, derErr := c.der()
+	if derErr != nil {
+		log.Fatalf("Failed to download der: %s", derErr)
+	}
 
 	ioutil.WriteFile("key.der", d, 0644)
+
+
+	certOut, err := os.Create("cert.pem")
+	if err != nil {
+		log.Fatalf("Failed to open cert.pem for writing: %s", err)
+	}
+	if err := pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: d}); err != nil {
+		log.Fatalf("Failed to write data to cert.pem: %s", err)
+	}
 }
